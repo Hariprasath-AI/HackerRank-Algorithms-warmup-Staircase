d2be5be0279bf34c254f3e400856ad34253f2fc8
Hariprasath G <hariprasath167@gmail.com>
2022-03-30 20:41:16 +0530

    HackerRank-Algorithms-warmup-Staircase

--- HackerRank-Algorithms-warmup-Staircase.py
+++ HackerRank-Algorithms-warmup-Staircase.py
@@ -1,3 +1,11 @@
+# Read this >>>
+# assign the single space to the variable
+# for loop goes n + 1 no.of times, 1 is the initial value and it is reduced with 'n', value stored in space_limit.
+# This means that how many spaces We have to allot infront of the letter '#'. eg: n = 6, at the start of for loop i = 1, space_limit = 6-1 = 5.
+# The value 5 is multiplied with the string 'space' and 5 spaces will be stored in variable 'solution'
+# Next, i multiplied with hash. This means that how many hash to print in the line stored in the variable hash_limit.
+# Just add those two variable and print. We will get right aligned staicase shaped output.  
+
 def Staircase(n):
     space = ' '
     for i in range(1, n+1):
